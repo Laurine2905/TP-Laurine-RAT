34cad79fed1f7bed05e2698bceeea828745dcc7d
Laurine2905 <laurine.rat@outlook.fr>
2022-10-19 11:38:43 +0200

    Ajout de classe

---
+++ Pancake.java
@@ -0,0 +1,9 @@
+public class Pancake {
+    private int nbPancake;
+    private int age;
+
+    public Pancake(int nbPancake,int age) {
+        this.age = age;
+        this.nbPancake = nbPancake;
+    }
+}
